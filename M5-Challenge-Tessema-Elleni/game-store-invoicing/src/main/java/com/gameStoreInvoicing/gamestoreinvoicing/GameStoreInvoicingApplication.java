762b30d9384fff5b5410b86f2ed4de7554b90397
Elleni Tessema <ellenitessema@Ellenis-MacBook-Pro.local>
2022-11-02 20:45:11 -0400

    adding game store invoicing

---
+++ M5-Challenge-Tessema-Elleni/game-store-invoicing/src/main/java/com/gameStoreInvoicing/gamestoreinvoicing/GameStoreInvoicingApplication.java
@@ -0,0 +1,13 @@
+package com.gameStoreInvoicing.gamestoreinvoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreInvoicingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreInvoicingApplication.class, args);
+	}
+
+}
---
+++ M5-Challenge-Tessema-Elleni/game-store-invoicing/src/test/java/com/gameStoreInvoicing/gamestoreinvoicing/GameStoreInvoicingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameStoreInvoicing.gamestoreinvoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreInvoicingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
